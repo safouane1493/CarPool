1895f8c2123aff975fcecacb6b58c5dbea2ebca6
Saf_One <safouane14@gmail.com>
2017-03-29 12:43:19 +0100

    app.js ajout chemin users

--- app.js
+++ app.js
@@ -5,9 +5,22 @@ const cors = require('cors');
 const passport= require('passport');
 const mongoose = require('mongoose');
 
+const users = require('./routes/users');
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
+
+app.use(bodyParser.json());
+
+app.user('/users',users);
+
+app.get('/',(req,res)=>{
+    res.send('Invalid EndPoint');
+});
+
+
 
 app.listen(port,()=>{
     console.log('Server started on port '+port);
