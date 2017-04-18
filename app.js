54e7f1a0b51ff45c20278c07ed8d5ddf29bb9eec
Saf_One <safouane14@gmail.com>
2017-04-18 21:30:24 +0100

    correction routes to route branch

--- app.js
+++ app.js
@@ -22,10 +22,5 @@ app.listen(port,()=>{
     console.log('Server started on port '+port);
 });
 
-app.user('/users',users);
-
-app.get('/',(req,res)=>{
-    res.send('Invalid EndPoint');
-});
 
 var cc ="hello i am a root";
