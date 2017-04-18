0c66f19fa175d932c1715646ffff3cb7b6ce25ba
Saf_One <safouane14@gmail.com>
2017-04-18 21:26:27 +0100

    route modification 2

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const passport= require('passport');
 const mongoose = require('mongoose');
 
-const users = require('./routes/users');
+
 
 const app = express();
 const port = 3000;
