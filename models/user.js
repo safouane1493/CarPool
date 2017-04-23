bccb4da55adf43ec7533dfbfe76cc1beb870a968
Saf_One <safouane14@gmail.com>
2017-04-23 23:47:00 +0100

    ajout models + userSchema

---
+++ models/user.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const config = require('../config/database');
+
+
+//User Schema
+const UserSchema = mongoose.Schema({
+    name : {
+        type: String,
+        required: true
+    },
+    lastname : {
+        type: String,
+        required: true
+    },
+    email : {
+        type: String,
+        required: true
+    },
+    password : {
+        type: String,
+        required: true
+    },
+    idcard : {
+        type: Number,
+        required: true
+    },
+    birthday : {
+        type: Date,
+        required: true
+    },
+    phone : {
+        type: Number,
+        required: true
+    }
+});
+
+const User = module.exports = mongoose.model('User', UserSchema);
+
+module.exports.getUserById = function(id, callback){
+    User.findById(id,callback);
+}
+
+module.exports.getUserByEmail = function(email, callback){
+    const query = {email : email}
+    User.findOne(query,callback);
+}
