ee595873b28f26a7e907b9a13b1c31b9fd19f238
Saf_One <safouane14@gmail.com>
2017-05-06 18:19:05 +0100

    login/logout works !

--- angular-src/src/app/components/login/login.component.ts
+++ angular-src/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from './../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  
+  email: String;
+  password: String;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
+  onLoginSubmit(){
+      const user = {
+        email: this.email,
+        password: this.password
+      }
+      this.authService.authenticateUser(user).subscribe(data => {
+        if(data.success){
+    console.log('login succeed');
+    this.authService.storeUserData(data.token, data.user);
+  }else{
+     console.log('login failed');
+  }
+      });
+  }
 }
--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -6,6 +6,7 @@ import 'rxjs/add/operator/map';
 export class AuthService {
 
   user: any;
+  authToken: any;
   constructor(private http:Http) { }
 
   registerUser(user){
@@ -13,4 +14,25 @@ export class AuthService {
     headers.append('Content-Type','application/json');
     return this.http.post('http://127.0.0.1:3000/users/register',user,{headers : headers}).map(res => res.json());
   }
+
+   authenticateUser(user){
+    let headers = new Headers();
+    headers.append('Content-Type','application/json');
+    return this.http.post('http://127.0.0.1:3000/users/authentification',user,{headers : headers}).map(res => res.json());
+  }
+  storeUserData(token, user){
+
+    localStorage.setItem('id_token', token);
+    localStorage.setItem('user', JSON.stringify(user));
+    this.authToken =token;
+    this.user = user;
+}
+
+logout(){
+    this.authToken = null;
+    this.user = null;
+
+    localStorage.clear();
+}
+
 }
